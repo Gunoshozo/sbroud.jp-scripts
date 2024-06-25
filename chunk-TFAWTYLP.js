90ec1d55757875b8b24d6319e46637219b45ed26
Gunoshozo <gunoshozo@gmail.com>
2024-06-25 19:10:49 +0400

    Auto-generated commit

---
+++ chunk-TFAWTYLP.js
@@ -0,0 +1,1 @@
+import{R as o,g as a}from"./chunk-VIQ542NQ.js";var n=(()=>{let t=class t{constructor(){this.loaderStateSubject=new a}setGlobalLoader(r){this.loaderStateSubject.next(r)}getLoaderState(){return this.loaderStateSubject.asObservable()}};t.\u0275fac=function(c){return new(c||t)},t.\u0275prov=o({token:t,factory:t.\u0275fac});let e=t;return e})();export{n as a};
