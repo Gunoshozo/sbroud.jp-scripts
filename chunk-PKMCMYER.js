27790a7b5595da268947d9c2d3137257bef834ca
Gunoshozo <gunoshozo@gmail.com>
2024-06-30 16:13:59 +0400

    Auto-generated commit

---
+++ chunk-PKMCMYER.js
@@ -0,0 +1,1 @@
+import{P as o,e as a}from"./chunk-F5MKLN4L.js";var n=(()=>{let t=class t{constructor(){this.loaderStateSubject=new a}setGlobalLoader(r){this.loaderStateSubject.next(r)}getLoaderState(){return this.loaderStateSubject.asObservable()}};t.\u0275fac=function(c){return new(c||t)},t.\u0275prov=o({token:t,factory:t.\u0275fac});let e=t;return e})();export{n as a};
